f008736699027ac01a3bcacf8fe741fa1a2f2adb
shaneyale <zeta186012@gmail.com>
2026-03-20 10:56:55 +0800

    feat(sort): add insertion sort in cpp and python

--- sort/cpp/sort.cpp
+++ sort/cpp/sort.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <algorithm>
 
+/**
+ * 冒泡排序
+ */
 void bubble_sort(std::vector<int> &v) {
     int n = v.size();
     for (int i = 0; i < n - 1; i++) {
@@ -13,6 +16,13 @@ void bubble_sort(std::vector<int> &v) {
     }
 }
 
+/**
+ * 选择排序
+ * 核心思想是把列表分为已经排序和没有排序的两个部分
+ * 在没有排序的部分中找最小的元素
+ * 找到的最小的元素和没有排序部分的第一个元素交换
+ * 然后将没有排序的部分向后移动一位，扩大已经排序的部分的范围
+ */
 void selection_sort(std::vector<int> &v) {
     int n = v.size();
     for (int i = 0; i < n - 1; i++) {
@@ -23,5 +33,22 @@ void selection_sort(std::vector<int> &v) {
             }
         }
         std::swap(v[i], v[min_idx]);
+    }
+}
+
+/**
+ * 插入排序
+ * 核心思想是将当前元素插入到已排序部分的正确位置
+ */
+void insertion_sort(std::vector<int> &v) {
+    int n = v.size();
+    for (int i = 1; i < n; ++i) {
+        int key = v[i];
+        int j = i - 1;
+        while (j >= 0 && v[j] > key) {
+            v[j + 1] = v[j];
+            --j;
+        }
+        v[j + 1] = key;
     }
 }
--- sort/py/sort.py
+++ sort/py/sort.py
@@ -19,3 +19,14 @@ def selection_sort(nums):
 
         nums[i], nums[min_idx] = nums[min_idx], nums[i]
     return nums
+
+def insertion_sort(nums):
+    n = len(nums)
+    for i in range(1, n):
+        key = nums[i]
+        j = i - 1
+        while j >= 0 and key < nums[j]:
+            nums[j + 1] = nums[j]
+            j -= 1
+        nums[j + 1] = key
+    return nums
